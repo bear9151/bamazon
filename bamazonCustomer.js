2327d053e00221d6ff1f1ec21cb4c22b507bd7d0
bear9151 <barrett.allen@icloud.com>
2017-04-23 16:42:55 -0400

    committing initial inquirer statment

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var fs = require("fs"),
     mysql = require("mysql"),
-    inquirer = require("inquirer");
+    inquirer = require("inquirer"),
+    chalk = require("chalk");
 
 // var for mySQL connection information
 var connection = mysql.createConnection({
@@ -14,5 +15,23 @@ var connection = mysql.createConnection({
 // initiating the mysql server and database, error catching and print connection ID
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    console.log("Connected as ID# " + connection.threadId);
+    console.log(chalk.bold.bgYellow("Welcome to Bamazon."));
+    console.log("Where you can find everything from B to Z. (Good Luck with the A's.)");
+    showProducts();
 });
+
+function showProducts() {
+    inquirer.prompt({
+        name: "showProducts",
+        type: "confirm",
+        message: "Would you like to see what's for sale?"
+    }).then(function(answer) {
+        if (answer.showProducts === true) {
+            console.log("products table");
+        } else {
+            console.log("Ok, Goodbye!");
+            process.exit();
+        }
+    })
+}
