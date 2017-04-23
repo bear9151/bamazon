0ee12b3bef7f043b9c645468840af1798f6dfdb0
bear9151 <barrett.allen@icloud.com>
2017-04-23 15:59:39 -0400

    committing working mysql connections

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,18 @@
 var fs = require("fs"),
+    mysql = require("mysql"),
     inquirer = require("inquirer");
+
+// var for mySQL connection information
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
+
+// initiating the mysql server and database, error catching and print connection ID
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
